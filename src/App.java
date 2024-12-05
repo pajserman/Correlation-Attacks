10d96798d57f8a883b61697364a399c56d47fe7e
Hannes Mansson <hannes.mansson01@gmail.com>
2024-12-05 20:34:58 +0100

    Done!

--- src/App.java
+++ src/App.java
@@ -1,7 +1,46 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        int[][] keys = new int[3][2];
+
         SingleAttack attack_1 = new SingleAttack(Stream.CD_1);
-       
+        keys[0] = attack_1.estimate_all_p();
+
+
+        SingleAttack attack_2 = new SingleAttack(Stream.CD_2);
+        keys[1] = attack_2.estimate_all_p();
+
+        SingleAttack attack_3 = new SingleAttack(Stream.CD_3);
+        keys[2] = attack_3.estimate_all_p();
+
+        System.out.println("Found keys are:");
+        for (int[] key : keys) {
+            System.out.println("Key: " + key[0] + " with prob: " + key[1] + "%");
+        }
+
+        System.out.println("Now testing if the give the same stream as Z...");
+
+        // keys[0][0] = 1;
+        
+        Stream streamChipher = new Stream(SingleAttack.toBinaryArray(keys[0][0]), SingleAttack.toBinaryArray(keys[1][0]), SingleAttack.toBinaryArray(keys[2][0]));
+        boolean check = true;
+        for (int i = 0; i < Stream.Z.length; i++) {
+            
+            if(streamChipher.getNext() != Stream.Z[i]) {
+                System.out.println("Wrong! The keys does not produce the same Z stream!");
+                check = false;
+                break;
+            }
+
+        }
+
+        if(check) {
+            System.out.println("The streams match up! The keys are correct!");
+        }
+
     }
+
+       
 }
--- src/SingleAttack.java
+++ src/SingleAttack.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SingleAttack {
 
     private double prob = 0.75;
@@ -9,7 +11,7 @@ public class SingleAttack {
         this.cd = cd;
     }
 
-    private int hamming_distance(int[] x, int[] y) {
+    public static double hamming_distance(int[] x, int[] y) {
         if(x.length != y.length) {
             return -1;
         }
@@ -23,9 +25,50 @@ public class SingleAttack {
 
     }
 
-    private double estimate_p(int[] x, int[] y) {
-        double p_prime = 1 - (hamming_distance(x, y))/Stream.Z.length;
+    private double estimate_one_p(int[] x, int[] y) {
+        double p_prime = 1 - ((hamming_distance(x, y))/x.length);
         return p_prime;
+    }
+
+    public int[] estimate_all_p() {
+        int [] ans = new int[2];
+        double largest_p = 0;
+        double K = Math.pow(2, cd.length-1);
+        int L = (int)K;
+        for(int i = 0; i < L; i++) {
+            LFSR lfsr = new LFSR(cd, 2, toBinaryArray(i));
+            int[] z2 = new int[z.length];
+            for(int j = 0; j < z.length; j++) {
+                z2[j] = lfsr.getNext();
+            }
+            double p = estimate_one_p(z, z2);
+            if(p > largest_p) {
+                largest_p = p;
+                ans[0] = i;
+                ans[1] = (int)(p*100);
+            }
+        }
+        return ans;
+    }
+
+         public static int[] toBinaryArray(int number) {
+        if (number == 0) {
+            return new int[]{0};
+        }
+
+        ArrayList<Integer> binaryList = new ArrayList<>();
+
+        while (number > 0) {
+            binaryList.add(0, number % 2);
+            number /= 2;
+        }
+
+        int[] binaryArray = new int[binaryList.size()];
+        for (int i = 0; i < binaryList.size(); i++) {
+            binaryArray[i] = binaryList.get(i);
+        }
+
+        return binaryArray;
     }
     
 }
